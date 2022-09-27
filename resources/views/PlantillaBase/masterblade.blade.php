a0c209adf5ed3b1340f82569b70d8271e0e072b7
Manuel Barreras <mbarreras.s@hotmail.com>
2022-09-27 15:21:37 -0700

    prueba cochito

--- resources/views/PlantillaBase/masterblade.blade.php
+++ resources/views/PlantillaBase/masterblade.blade.php
@@ -37,7 +37,7 @@
                                 <a class="nav-link d-flex align-items-center text-white text-decoration-none dropdown-toggle"
                                     href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown"
                                     aria-expanded="false">
-                                    <strong>Iniciar Sesión</strong>
+                                    <strong>Iniciar Sesión del cochi</strong>
                                 </a>
                                 <ul class="dropdown-menu dropdown-menu-dark text-small shadow"
                                     aria-labelledby="navbarDropdownMenuLink">
