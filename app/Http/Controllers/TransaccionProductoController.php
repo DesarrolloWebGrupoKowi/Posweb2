cfc38aa618825caf8829f1d6f706407d77a66bf8
Manuel Barreras <mbarreras.s@hotmail.com>
2022-10-03 18:11:29 -0700

    modulo de transacciones de producto listo, nota: revisar transacciones

--- app/Http/Controllers/TransaccionProductoController.php
+++ app/Http/Controllers/TransaccionProductoController.php
@@ -8,9 +8,11 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\Articulo;
 use App\Models\Tienda;
 use App\Models\TransaccionTienda;
-use App\Models\DatInventario;
+use App\Models\InventarioTienda;
 use App\Models\CapRecepcion;
 use App\Models\DatRecepcion;
+use App\Models\MovimientoProducto;
+use App\Models\HistorialMovimientoProducto;
 
 class TransaccionProductoController extends Controller
 {
@@ -56,21 +58,20 @@ class TransaccionProductoController extends Controller
         $idTiendaDestino = $request->idTiendaDestino;
         $codsArticulo = $request->CodArticulo;
         
+        $tienda = Tienda::where('IdTienda', $idTiendaDestino)
+        ->select('NomTienda', 'Almacen')
+        ->first();
+        
         try {
             DB::beginTransaction();
 
-            $tienda = Tienda::where('IdTienda', $idTiendaDestino)
-                ->select('NomTienda', 'Almacen')
-                ->first();
-
             $capRecepcion = new CapRecepcion();
-            $capRecepcion->FechaLlegada = date('d-m-y H:i:s');
+            $capRecepcion->FechaLlegada = date('d-m-Y H:i:s');
             $capRecepcion->PackingList = $tienda->NomTienda;
             $capRecepcion->Almacen = $tienda->Almacen;
             $capRecepcion->IdStatusRecepcion = 1;
             $capRecepcion->IdUsuario = Auth::user()->IdUsuario;
             $capRecepcion->save();
-            
 
             foreach ($codsArticulo as $keyCodArticulo => $cantArticulo) {
                 DatRecepcion::insert([
@@ -79,14 +80,35 @@ class TransaccionProductoController extends Controller
                     'CantEnviada' => $cantArticulo,
                     'IdStatusRecepcion' => 1
                 ]);
-            }
 
-            DB::commit();
+                HistorialMovimientoProducto::insert([
+                    'IdTienda' => Auth::user()->usuarioTienda->IdTienda,
+                    'CodArticulo' => $keyCodArticulo,
+                    'CantArticulo' => -$cantArticulo,
+                    'FechaMovimiento' => date('d-m-Y H:i:s'),
+                    'Referencia' => $tienda->NomTienda,
+                    'IdMovimiento' => 2,
+                    'IdUsuario' => Auth::user()->IdUsuario
+                ]);
 
-            return back()->with('msjAdd', 'Transferencia Exitosa!');
+                $stockArticulo = InventarioTienda::where('IdTienda', Auth::user()->usuarioTienda->IdTienda)
+                    ->where('CodArticulo', $keyCodArticulo)
+                    ->value('StockArticulo');
+
+                InventarioTienda::where('IdTienda', Auth::user()->usuarioTienda->IdTienda)
+                    ->where('CodArticulo', ''.$keyCodArticulo.'')
+                    ->update([
+                        'StockArticulo' => $stockArticulo - $cantArticulo
+                    ]);
+
+                }
+                
+                DB::commit();
+                return back()->with('msjAdd', 'Transferencia Exitosa!');
         } catch (\Throwable $th) {
             DB::rollback();
-            return back()->with('msjdelete', 'Error: ' . $th->getMessage());
+            //return back()->with('msjdelete', 'Error: ' . $th->getMessage());
+            return $th;
         }
 
     }
