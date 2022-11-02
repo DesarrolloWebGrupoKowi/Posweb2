b2274d96b39bf364308db3448a2ee6626ec20263
Manuel Barreras <mbarreras.s@hotmail.com>
2022-11-02 08:37:55 -0700

    caprecepcion

--- resources/views/Recepcion/ModalCancelarRecepcion.blade.php
+++ resources/views/Recepcion/ModalCancelarRecepcion.blade.php
@@ -1,5 +1,5 @@
 <!-- Modal Cancelar Recepción-->
-<div class="modal fade" id="ModalCancelarRecepcion{{ $rTienda->IdCapRecepcion }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="ModalCancelarRecepcion{{ $rTienda->IdRecepcion }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
