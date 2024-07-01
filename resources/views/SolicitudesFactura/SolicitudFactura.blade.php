6f00e6ad2fedaf709c4a9ae44bd96105ed8c72f1
Daniel Hernandez <hernandezdany41@gmail.com>
2024-07-01 09:13:59 -0700

    agregando diseño vista solicitud de factura

--- resources/views/SolicitudesFactura/SolicitudFactura.blade.php
+++ resources/views/SolicitudesFactura/SolicitudFactura.blade.php
@@ -2,163 +2,171 @@
 @section('title', 'Solicitud Factura ' . $solicitud->IdSolicitudFactura)
 @section('dashboardWidth', 'width-general')
 @section('contenido')
-    <div class="container-fluid pt-4 width-general">
-        <div class="d-flex justify-content-sm-between align-items-sm-center flex-column flex-sm-row pb-2">
-            @include('components.title', [
-                'titulo' => 'Solicitud Factura',
-                'options' => [
-                    [
-                        'name' => 'Solicitudes Factura',
-                        'value' => '/SolicitudesFactura',
+    <div class="container-fluid width-general d-flex flex-column gap-4 pt-4">
+
+        <div class="card border-0 p-4" style="border-radius: 10px">
+            <div class="d-flex justify-content-sm-between align-items-sm-end flex-column flex-sm-row">
+                @include('components.title', [
+                    'titulo' => 'Solicitud Factura',
+                    'options' => [
+                        [
+                            'name' => 'Solicitudes Factura',
+                            'value' => '/SolicitudesFactura',
+                        ],
                     ],
-                ],
-            ])
-            <div class="">
-                @if ($solicitud['ConstanciaSituacionFiscal'] != null)
-                    <a href="{{ '/VerConstanciaCliente/' . $solicitud->IdSolicitudFactura }}" type="button"
-                        class="btn btn-sm btn-dark" target="_blank" title="Finalizar solicitud">
-                        <i class="fa fa-book"></i> Ver constancia
-                    </a>
-                @endif
-                @if ($solicitud->Bill_To != null)
-                    <a href="{{ '/SolicitudesFactura/Finalizar/' . $solicitud->Id }}" type="button"
-                        class="btn btn-sm btn-dark" title="Finalizar solicitud">
-                        <i class="fa fa-check" aria-hidden="true"></i> Finalizar solicitud
-                    </a>
-                @endif
+                ])
+                <div class="d-flex gap-2">
+                    @if ($solicitud['ConstanciaSituacionFiscal'] != null)
+                        <a href="{{ '/VerConstanciaCliente/' . $solicitud->IdSolicitudFactura }}" type="button"
+                            class="btn btn-sm btn-dark" target="_blank" title="Finalizar solicitud">
+                            <i class="fa fa-book"></i> Ver constancia
+                        </a>
+                    @endif
+                    @if ($solicitud->Bill_To != null)
+                        <a href="{{ '/SolicitudesFactura/Finalizar/' . $solicitud->Id }}" type="button"
+                            class="btn btn-sm btn-dark" title="Finalizar solicitud">
+                            <i class="fa fa-check" aria-hidden="true"></i> Finalizar solicitud
+                        </a>
+                    @endif
+                </div>
+            </div>
+
+            <div>
+                @include('Alertas.Alertas')
             </div>
         </div>
 
-        <div>
-            @include('Alertas.Alertas')
-        </div>
-
-        <div class="content-table content-table-full card p-4 mb-4" style="border-radius: 20px">
+        <div class="content-table content-table-flex-none content-table-full card border-0 p-4" style="border-radius: 10px">
             <div class="row g-3">
                 <div class="col-md-3">
-                    <label class="form-label fw-bold m-0 text-secondary">Id</label>
-                    <input type="text" class="form-control" value="{{ $solicitud->IdSolicitudFactura }}" disabled>
+                    <label class="form-label text-secondary m-0" style="font-weight:500">Id</label>
+                    <input type="text" class="form-control rounded" value="{{ $solicitud->IdSolicitudFactura }}"
+                        disabled>
                 </div>
                 <div class="col-md-3">
-                    <label for="inputPassword4" class="form-label text-secondary fw-bold m-0">Id Cliente Cloud </label>
-                    <input type="text" class="form-control"
+                    <label class="form-label text-secondary m-0" style="font-weight:500">Id Cliente Cloud </label>
+                    <input type="text" class="form-control rounded"
                         value="{{ $solicitud->IdClienteCloud ? $solicitud->IdClienteCloud : 'Sin dato' }}" disabled>
                 </div>
                 <div class="col-md-3">
-                    <label for="inputPassword4" class="form-label text-secondary fw-bold m-0">Id Usuario Cliente </label>
-                    <input type="text" class="form-control"
+                    <label class="form-label text-secondary m-0" style="font-weight:500">Id Usuario Cliente </label>
+                    <input type="text" class="form-control rounded"
                         value="{{ $solicitud->IdUsuarioCliente ? $solicitud->IdUsuarioCliente : 'Sin dato' }}" disabled>
                 </div>
                 <div class="col-md-3 ">
-                    <label for="inputPassword4" class="form-label fw-bold text-secondary m-0">Fecha</label>
-                    <input type="text" class="form-control" value="{{ $solicitud->FechaSolicitud }}" disabled>
+                    <label class="form-label text-secondary m-0" style="font-weight:500">Fecha</label>
+                    <input type="text" class="form-control rounded" value="{{ $solicitud->FechaSolicitud }}" disabled>
                 </div>
                 <div class="col-md-3">
-                    <label for="inputPassword4" class="form-label text-secondary fw-bold m-0">Nombre Cliente </label>
-                    <input type="text" class="form-control" value="{{ $solicitud->NomCliente }}" disabled>
+                    <label class="form-label text-secondary m-0" style="font-weight:500">Nombre Cliente </label>
+                    <input type="text" class="form-control rounded" value="{{ $solicitud->NomCliente }}" disabled>
                 </div>
                 <div class="col-md-3">
-                    <label for="inputPassword4" class="form-label text-secondary fw-bold m-0">Teléfono</label>
-                    <input type="text" class="form-control" value="{{ $solicitud->Telefono }}" disabled>
+                    <label class="form-label text-secondary m-0" style="font-weight:500">Teléfono</label>
+                    <input type="text" class="form-control rounded" value="{{ $solicitud->Telefono }}" disabled>
                 </div>
                 <div class="col-md-3">
-                    <label for="inputPassword4" class="form-label text-secondary fw-bold m-0">Correo</label>
-                    <input type="text" class="form-control" value="{{ $solicitud->Email }}" disabled>
+                    <label class="form-label text-secondary m-0" style="font-weight:500">Correo</label>
+                    <input type="text" class="form-control rounded" value="{{ $solicitud->Email }}" disabled>
                 </div>
                 <div class="col-md-3">
-                    <label for="inputPassword4" class="form-label text-secondary fw-bold m-0">RFC</label>
-                    <input type="text" class="form-control" value="{{ $solicitud->RFC }}" disabled>
+                    <label class="form-label text-secondary m-0" style="font-weight:500">RFC</label>
+                    <input type="text" class="form-control rounded" value="{{ $solicitud->RFC }}" disabled>
                 </div>
                 <div class="col-md-3">
-                    <label for="inputPassword4" class="form-label text-secondary fw-bold m-0">Tipo Persona </label>
-                    <input type="text" class="form-control" value="{{ $solicitud->TipoPersona }}" disabled>
+                    <label class="form-label text-secondary m-0" style="font-weight:500">Tipo Persona </label>
+                    <input type="text" class="form-control rounded" value="{{ $solicitud->TipoPersona }}" disabled>
                 </div>
                 <div class="col-md-3">
-                    <label for="inputPassword4" class="form-label text-secondary fw-bold m-0">Calle</label>
-                    <input type="text" class="form-control" value="{{ $solicitud->Calle }}" disabled>
+                    <label class="form-label text-secondary m-0" style="font-weight:500">Calle</label>
+                    <input type="text" class="form-control rounded" value="{{ $solicitud->Calle }}" disabled>
                 </div>
                 <div class="col-md-3">
-                    <label for="inputPassword4" class="form-label text-secondary fw-bold m-0">Número Exterior </label>
-                    <input type="text" class="form-control" value="{{ $solicitud->NumExt }}" disabled>
+                    <label class="form-label text-secondary m-0" style="font-weight:500">Número Exterior </label>
+                    <input type="text" class="form-control rounded" value="{{ $solicitud->NumExt }}" disabled>
                 </div>
                 <div class="col-md-3">
-                    <label for="inputPassword4" class="form-label text-secondary fw-bold m-0">Número Interior </label>
-                    <input type="text" class="form-control" value="{{ $solicitud->NumInt }}" disabled>
+                    <label class="form-label text-secondary m-0" style="font-weight:500">Número Interior </label>
+                    <input type="text" class="form-control rounded" value="{{ $solicitud->NumInt }}" disabled>
                 </div>
                 <div class="col-md-3">
-                    <label for="inputPassword4" class="form-label text-secondary fw-bold m-0">Colonia</label>
-                    <input type="text" class="form-control" value="{{ $solicitud->Colonia }}" disabled>
+                    <label class="form-label text-secondary m-0" style="font-weight:500">Colonia</label>
+                    <input type="text" class="form-control rounded" value="{{ $solicitud->Colonia }}" disabled>
                 </div>
                 <div class="col-md-3">
-                    <label for="inputPassword4" class="form-label text-secondary fw-bold m-0">Codigo Postal </label>
-                    <input type="text" class="form-control" value="{{ $solicitud->CodigoPostal }}" disabled>
+                    <label class="form-label text-secondary m-0" style="font-weight:500">Codigo Postal </label>
+                    <input type="text" class="form-control rounded" value="{{ $solicitud->CodigoPostal }}" disabled>
                 </div>
                 <div class="col-md-3">
-                    <label for="inputPassword4" class="form-label text-secondary fw-bold m-0">Municipio </label>
-                    <input type="text" class="form-control" value="{{ $solicitud->Municipio }}" disabled>
+                    <label class="form-label text-secondary m-0" style="font-weight:500">Municipio </label>
+                    <input type="text" class="form-control rounded" value="{{ $solicitud->Municipio }}" disabled>
                 </div>
                 <div class="col-md-3">
-                    <label for="inputPassword4" class="form-label text-secondary fw-bold m-0">Ciudad</label>
-                    <input type="text" class="form-control" value="{{ $solicitud->Ciudad }}" disabled>
+                    <label class="form-label text-secondary m-0" style="font-weight:500">Ciudad</label>
+                    <input type="text" class="form-control rounded" value="{{ $solicitud->Ciudad }}" disabled>
                 </div>
                 <div class="col-md-3">
-                    <label for="inputPassword4" class="form-label text-secondary fw-bold m-0">Estado</label>
-                    <input type="text" class="form-control" value="{{ $solicitud->Estado }}" disabled>
+                    <label class="form-label text-secondary m-0" style="font-weight:500">Estado</label>
+                    <input type="text" class="form-control rounded" value="{{ $solicitud->Estado }}" disabled>
                 </div>
                 <div class="col-md-3">
-                    <label for="inputPassword4" class="form-label text-secondary fw-bold m-0">País</label>
-                    <input type="text" class="form-control" value="{{ $solicitud->Pais }}" disabled>
+                    <label class="form-label text-secondary m-0" style="font-weight:500">País</label>
+                    <input type="text" class="form-control rounded" value="{{ $solicitud->Pais }}" disabled>
                 </div>
                 <div class="col-md-3">
-                    <label for="inputPassword4" class="form-label text-secondary fw-bold m-0">Acción</label>
-                    <input type="text" class="form-control" value="{{ $solicitud->Editar ? 'Actualizar' : 'Nuevo' }}"
-                        disabled>
+                    <label class="form-label text-secondary m-0" style="font-weight:500">Acción</label>
+                    <input type="text" class="form-control rounded"
+                        value="{{ $solicitud->Editar ? 'Actualizar' : 'Nuevo' }}" disabled>
                 </div>
             </div>
         </div>
 
-        <div>
-            <h2>Detalle de la Solicitud </h2>
+        {{-- <div class="content-table content-table-flex-none content-table-full card border-0 p-4"
+            style="border-radius: 10px">
+        </div> --}}
 
-        </div>
-
-        <div class="content-table content-table-full card p-4" style="border-radius: 20px">
+        <div class="content-table content-table-flex-none content-table-full card border-0 p-4"
+            style="border-radius: 10px">
+            {{-- <h2>Detalle de la Solicitud </h2> --}}
+            <span class="fs-5 text-sm mb-2" style="font-weight: 500; font-family: sans-serif; color: #334155">
+                Detalle de la Solicitud
+            </span>
             <div class="row g-3">
                 <div class="col-md-3">
-                    <label class="form-label fw-bold m-0 text-secondary">Tienda</label>
-                    <input type="text" class="form-control" value="{{ $solicitud->NomTienda }}" disabled>
+                    <label class="form-label text-secondary m-0" style="font-weight:500">Tienda</label>
+                    <input type="text" class="form-control rounded" value="{{ $solicitud->NomTienda }}" disabled>
                 </div>
                 <div class="col-md-3 ">
-                    <label for="inputPassword4" class="form-label fw-bold text-secondary m-0">Tipo De Pago </label>
-                    <input type="text" class="form-control" value="{{ $solicitud->NomTipoPago }}" disabled>
+                    <label class="form-label text-secondary m-0" style="font-weight:500">Tipo De Pago </label>
+                    <input type="text" class="form-control rounded" value="{{ $solicitud->NomTipoPago }}" disabled>
                 </div>
                 <div class="col-md-3 ">
-                    <label for="inputPassword4" class="form-label fw-bold text-secondary m-0">Banco </label>
-                    <input type="text" class="form-control" value="{{ $solicitud->NomBanco }}" disabled>
+                    <label class="form-label text-secondary m-0" style="font-weight:500">Banco </label>
+                    <input type="text" class="form-control rounded" value="{{ $solicitud->NomBanco }}" disabled>
                 </div>
                 <div class="col-md-3 ">
-                    <label for="inputPassword4" class="form-label fw-bold text-secondary m-0">Numero Tarjeta </label>
-                    <input type="text" class="form-control" value="{{ $solicitud->NumTarjeta }}" disabled>
+                    <label class="form-label text-secondary m-0" style="font-weight:500">Numero Tarjeta </label>
+                    <input type="text" class="form-control rounded" value="{{ $solicitud->NumTarjeta }}" disabled>
                 </div>
                 <div class="col-md-3">
-                    <label for="inputPassword4" class="form-label text-secondary fw-bold m-0">Bill To </label>
-                    <input type="text" class="form-control"
+                    <label class="form-label text-secondary m-0" style="font-weight:500">Bill To </label>
+                    <input type="text" class="form-control rounded"
                         value="{{ $solicitud->Bill_To ? $solicitud->Bill_To : 'Sin dato' }}" disabled>
                 </div>
                 <div class="col-md-3">
-                    <label for="inputPassword4" class="form-label text-secondary fw-bold m-0">Uso CFDI </label>
-                    <input type="text" class="form-control"
+                    <label class="form-label text-secondary m-0" style="font-weight:500">Uso CFDI </label>
+                    <input type="text" class="form-control rounded"
                         value="{{ $solicitud->UsoCFDI ? $solicitud->UsoCFDI : 'Sin dato' }}" disabled>
                 </div>
                 <div class="col-md-3">
-                    <label for="inputPassword4" class="form-label text-secondary fw-bold m-0">Metódo Pago </label>
-                    <input type="text" class="form-control"
+                    <label class="form-label text-secondary m-0" style="font-weight:500">Metódo Pago </label>
+                    <input type="text" class="form-control rounded"
                         value="{{ $solicitud->MetodoPago ? $solicitud->MetodoPago : 'Sin dato' }}" disabled>
                 </div>
             </div>
 
             {{-- <div class="card p-4 mt-4"> --}}
-            <div class="content-table mt-4 content-table-full card p-4">
+            <div class="content-table mt-4 content-table-full card">
                 <table>
                     <thead class="table-head">
                         <tr>
@@ -188,13 +196,10 @@
                                     <td>{{ $cliente->Locacion }}</td>
                                     <td>{{ $cliente->Ciudad }}</td>
                                     <td>
-                                        {{-- <a href="/" class= "btn btn-dark-outline">Relacionar</a> --}}
-                                        <div class="input-group-append">
-                                            <a href="{{ '/SolicitudesFactura/Relacionar/' . $solicitud->Id . '/' . $cliente->Bill_To }}"
-                                                class="input-group-text text-decoration-none">
-                                                Relacionar
-                                            </a>
-                                        </div>
+                                        <a href="{{ '/SolicitudesFactura/Relacionar/' . $solicitud->Id . '/' . $cliente->Bill_To }}"
+                                            class="btn-table">
+                                            Relacionar
+                                        </a>
                                     </td>
                                 </tr>
                             @endforeach
