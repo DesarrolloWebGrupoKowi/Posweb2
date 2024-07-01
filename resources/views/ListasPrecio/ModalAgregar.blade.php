7fdcee6643f7a7ebf489eab5a0d2eb0d41149dc6
Daniel Hernandez <hernandezdany41@gmail.com>
2024-07-01 10:10:27 -0700

    agregando diseño a vista lista de precios

--- resources/views/ListasPrecio/ModalAgregar.blade.php
+++ resources/views/ListasPrecio/ModalAgregar.blade.php
@@ -1,29 +1,32 @@
 <!-- Modal Agregar-->
 <div class="modal fade" id="ModalAgregar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
-        <div class="modal-content">
+        <div class="modal-content border-0">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Agregar Lista de Precio</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <form action="/CrearListaPrecio" method="POST">
                     @csrf
                     <div class="mb-3">
                         <label for="" class="form-label">Lista de Precio</label>
-                        <input type="text" id="NomListaPrecio" name="NomListaPrecio" class="form-control" onkeyup="mayusculas(this)" required>
+                        <input type="text" id="NomListaPrecio" name="NomListaPrecio" class="form-control rounded"
+                            onkeyup="mayusculas(this)" required>
                     </div>
                     <div class="mb-3">
                         <label for="" class="form-label">Peso Minimo</label>
-                        <input class="form-control" type="number" name="PesoMinimo" id="PesoMinimo" step="0.01" required>
+                        <input class="form-control rounded" type="number" name="PesoMinimo" id="PesoMinimo"
+                            step="0.01" required>
                     </div>
                     <div class="mb-3">
                         <label for="" class="form-label">Peso Maximo</label>
-                        <input class="form-control" type="number" name="PesoMaximo" id="PesoMaximo" step="0.01" required>
+                        <input class="form-control rounded" type="number" name="PesoMaximo" id="PesoMaximo"
+                            step="0.01" required>
                     </div>
                     <div class="mb-3">
                         <label for="" class="form-label">Iva</label>
-                        <input class="form-control" type="number" name="PorcentajeIva" id="PorcentajeIva" step="0.01" required>
+                        <input class="form-control rounded" type="number" name="PorcentajeIva" id="PorcentajeIva"
+                            step="0.01" required>
                     </div>
                     <div class="input-group mb-3">
                         <span class="input-group-text">
@@ -34,18 +37,15 @@
                     <div id="divSelectListaPrecio" class="mb-3" style="display: none">
                         <select class="form-select" name="selectListaPrecio" id="selectListaPrecio">
                             @foreach ($selectListaPrecio as $listaPrecio)
-                                <option value="{{$listaPrecio->IdListaPrecio}}">{{$listaPrecio->NomListaPrecio}}</option>
+                                <option value="{{ $listaPrecio->IdListaPrecio }}">{{ $listaPrecio->NomListaPrecio }}
+                                </option>
                             @endforeach
                         </select>
                     </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">
-                    <i class="fa fa-close"></i> Cerrar
-                </button>
-                <button type="submit" class="btn btn-warning">
-                    <i class="fa fa-save"></i> Crear
-                </button>
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar </button>
+                <button type="submit" class="btn btn-warning">Crear </button>
                 </form>
             </div>
         </div>
