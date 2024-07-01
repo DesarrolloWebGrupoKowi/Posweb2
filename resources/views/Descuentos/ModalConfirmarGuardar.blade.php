21b2bd8ece61867972bc82d4871acf532f63126c
Daniel Hernandez <hernandezdany41@gmail.com>
2024-07-01 15:05:52 -0700

    agregando diseño a modulo de descuentos

--- resources/views/Descuentos/ModalConfirmarGuardar.blade.php
+++ resources/views/Descuentos/ModalConfirmarGuardar.blade.php
@@ -1,31 +1,21 @@
 <!--MODAL CONFIRMAR GUARDAR EDICION DE PAQUETE-->
-<div class="modal fade" data-bs-backdrop="static" id="ModalConfirmarGuardar" tabindex="-1"
-    aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="ModalConfirmarGuardar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
-        <div class="modal-content">
+        <div class="modal-content border-0">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Editar Paquete</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <div class="d-flex justify-content-center">
-                    <div class="col-auto">
-                        <h5>¿Desea Editar el Paquete?</h5>
-                    </div>
-                </div>
-                <div class="d-flex justify-content-center">
-                    <div class="col-auto">
-                        <h4>{{ $descuento->NomDescuento }}</h4>
-                    </div>
-                </div>
+                <p class="fs-6 text-center fw-normal text-secondary m-0" style="line-height: 24px">
+                    ¿Desea Editar el Paquete?
+                </p>
+                <p class="fs-6 text-center fw-normal text-secondary m-0" style="line-height: 24px">
+                    {{ $descuento->NomDescuento }}
+                </p>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-sm btn-danger" data-bs-dismiss="modal">
-                    <i class="fa fa-close"></i> Cerrar
-                </button>
-                <button id="btnEditarPaquete" class="btn btn-sm btn-warning">
-                    <i class="fa fa-save"></i> Guardar
-                </button>
+                <button type="button" class="btn btn-sm btn-danger" data-bs-dismiss="modal">Cerrar </button>
+                <button id="btnEditarPaquete" class="btn btn-sm btn-warning">Guardar </button>
             </div>
         </div>
     </div>
--- resources/views/Descuentos/ModalEliminarConfirm.blade.php
+++ resources/views/Descuentos/ModalEliminarConfirm.blade.php
@@ -1,29 +1,24 @@
 <!-- Modal Confirmacion Eliminar-->
-<div class="modal fade" data-bs-backdrop="static" id="ModalEliminarConfirm{{ $descuento->IdEncDescuento }}" tabindex="-1"
+<div class="modal fade" id="ModalEliminarConfirm{{ $descuento->IdEncDescuento }}" tabindex="-1"
     aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
-        <div class="modal-content">
+        <div class="modal-content border-0">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Solicitud de Eliminación</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <div class="d-flex justify-content-center">
-                    <h5>¿Seguro Desea Eliminar este Descuento   ?</h5>
-                </div>
-                <div class="d-flex justify-content-center">
-                    <h3>{{ $descuento->NomDescuento }}</h3>
-                </div>
+                <p class="fs-6 text-center fw-normal text-secondary m-0" style="line-height: 24px">
+                    ¿Seguro Desea Eliminar este Descuento?
+                </p>
+                <p class="fs-6 text-center fw-normal text-secondary m-0" style="line-height: 24px">
+                    {{ $descuento->NomDescuento }}
+                </p>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-sm btn-warning" data-bs-dismiss="modal">
-                    <i class="fa fa-close"></i> Cerrar
-                </button>
-                <form action="EliminarDescuento/{{ $descuento->IdEncDescuento }}" method="POST">
+                <button type="button" class="btn btn-sm btn-warning" data-bs-dismiss="modal">Cerrar </button>
+                <form class="d-flex" action="EliminarDescuento/{{ $descuento->IdEncDescuento }}" method="POST">
                     @csrf
-                    <button class="btn btn-sm btn-danger">
-                        <i class="fa fa-trash-o"></i> Eliminar
-                    </button>
+                    <button class="btn btn-sm btn-danger">Eliminar </button>
                 </form>
             </div>
         </div>
