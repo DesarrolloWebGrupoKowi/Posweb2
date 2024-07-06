9995da8e717dda40ef813a7ba35adff6452c2ca2
Daniel Hernandez <hernandezdany41@gmail.com>
2024-07-06 12:03:12 -0700

    modificando vistas de usuarios

--- resources/views/Usuarios/ModalActivarUsuario.blade.php
+++ resources/views/Usuarios/ModalActivarUsuario.blade.php
@@ -1,25 +1,26 @@
 <!--Modal Activar Usuario-->
 
-<div class="modal fade" id="modalActivarUsuario{{$usuario->IdUsuario}}" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalToggleLabel2">Confirmar Usuario</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-      <form action="ActivarUsuario/{{$usuario->IdUsuario}}" method="POST">
-            @csrf
-       <div class="mb-3">
-        <label for="passAdmin" class="form-label">Contraseña</label>
-            <input type="password" id="passAdmin" name="passAdmin" class="form-control" tabindex="3" placeholder="Contraseña" required>
-        </div>
-      </div>
-      <div class="modal-footer">
-            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
-            <input type="submit" class="btn btn-warning" value="Activar Usuario">
-            </form>
+<div class="modal fade" id="modalActivarUsuario{{ $usuario->IdUsuario }}" aria-hidden="true"
+    aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content border-0">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalToggleLabel2">Activar usuario</h5>
+            </div>
+            <div class="modal-body">
+                <form action="ActivarUsuario/{{ $usuario->IdUsuario }}" method="POST">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="passAdmin" class="form-label">Contraseña</label>
+                        <input type="password" id="passAdmin" name="passAdmin" class="form-control rounded"
+                            style="line-height: 18px" tabindex="3" placeholder="Contraseña" required>
+                    </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-sm btn-danger" data-bs-dismiss="modal">Cerrar</button>
+                <input type="submit" class="btn btn-sm btn-warning" value="Activar Usuario">
+                </form>
+            </div>
         </div>
     </div>
-  </div>
 </div>
--- resources/views/Usuarios/ModalEditar.blade.php
+++ resources/views/Usuarios/ModalEditar.blade.php
@@ -1,42 +1,42 @@
 <!-- Modal Editar-->
-<div class="modal fade" id="ModalEditar{{$usuario->IdUsuario}}-{{$usuario->NomUsuario}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Editar Usuario: {{$usuario->NomUsuario}}</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                
-                <div class="modal-body">
-                    <form action="Editar/{{$usuario->IdUsuario}}" method="POST">
-                        @csrf
-                        <div class="mb-3">
-                            <label for="" class="form-label">Numero de Nomina</label>
-                            <input type="text" id="NumNomina" name="NumNomina" class="form-control" tabindex="2" required value="{{$usuario->NumNomina}}">
-                        </div>
-                        <div class="mb-3">
-                            <label for="" class="form-label">Correo</label>
-                            <input type="email" id="Correo" name="Correo" class="form-control" tabindex="4" required value="{{$usuario->Correo}}">
-                        </div>
-                        <div class="mb-3">
-                            <label for="" class="form-label">Tipo Usuario</label>
-                            <select name="IdTipoUsuario" id="IdTipoUsuario" class="form-select">
-                                @foreach($tipoUsuarios as $tipoUsuario)
-                                <option {!! $usuario->IdTipoUsuario == $tipoUsuario->IdTipoUsuario ? 'selected' : '' !!} value="{{$tipoUsuario->IdTipoUsuario}}">{{$tipoUsuario->NomTipoUsuario}}</option>
-                                @endforeach
-                            </select>
-                        </div>
+<div class="modal fade" id="ModalEditar{{ $usuario->IdUsuario }}-{{ $usuario->NomUsuario }}" tabindex="-1"
+    aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content border-0">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Editar Usuario: {{ $usuario->NomUsuario }}</h5>
+            </div>
 
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">
-                        <i class="fa fa-close"></i> Cerrar
-                    </button>
-                    <button type="submit" class="btn btn-warning">
-                        <i class="fa fa-save"></i> Editar Usuario
-                    </button>
-                    </form>
-                </div>
+            <div class="modal-body">
+                <form action="Editar/{{ $usuario->IdUsuario }}" method="POST">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="" class="form-label">Numero de Nomina</label>
+                        <input type="text" id="NumNomina" name="NumNomina" class="form-control rounded"
+                            style="line-height: 18px" tabindex="2" required value="{{ $usuario->NumNomina }}">
+                    </div>
+                    <div class="mb-3">
+                        <label for="" class="form-label">Correo</label>
+                        <input type="email" id="Correo" name="Correo" class="form-control rounded"
+                            style="line-height: 18px" tabindex="4" required value="{{ $usuario->Correo }}">
+                    </div>
+                    <div class="mb-3">
+                        <label for="" class="form-label">Tipo Usuario</label>
+                        <select name="IdTipoUsuario" id="IdTipoUsuario" class="form-select rounded"
+                            style="line-height: 18px">
+                            @foreach ($tipoUsuarios as $tipoUsuario)
+                                <option {!! $usuario->IdTipoUsuario == $tipoUsuario->IdTipoUsuario ? 'selected' : '' !!} value="{{ $tipoUsuario->IdTipoUsuario }}">
+                                    {{ $tipoUsuario->NomTipoUsuario }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-sm btn-danger" data-bs-dismiss="modal">Cerrar </button>
+                <button type="submit" class="btn btn-sm btn-warning">Editar Usuario </button>
+                </form>
             </div>
         </div>
     </div>
+</div>
