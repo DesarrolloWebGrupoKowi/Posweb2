b258841ab87ad9622bb55b882f2e77edf1f79c0c
Daniel Hernandez <hernandezdany41@gmail.com>
2024-07-08 09:17:48 -0700

    modificando vistas de tipo de menus

--- resources/views/TipoMenu/ModalAgregar.blade.php
+++ resources/views/TipoMenu/ModalAgregar.blade.php
@@ -1,27 +1,23 @@
 <!-- Modal Agregar-->
 <div class="modal fade" id="ModalAgregar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
-        <div class="modal-content">
+        <div class="modal-content border-0">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Agregar Tipo de Menu</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <form action="/CrearTipoMenu" method="POST">
                     @csrf
                     <div class="mb-3">
                         <label for="" class="form-label">Tipo de Menu</label>
-                        <input type="text" id="NomTipoMenu" name="NomTipoMenu" class="form-control" placeholder="Nombre" onkeyup="mayusculas(this)" required>
+                        <input type="text" id="NomTipoMenu" name="NomTipoMenu" class="form-control rounded"
+                            style="line-height: 18px" placeholder="Nombre" onkeyup="mayusculas(this)" required>
                     </div>
 
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">
-                    <i class="fa fa-close"></i> Cerrar
-                </button>
-                <button class="btn btn-warning">
-                    <i class="fa fa-save"></i> Agregar
-                </button>
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar </button>
+                <button class="btn btn-warning">Agregar </button>
                 </form>
             </div>
         </div>
--- resources/views/TipoMenu/ModalEditar.blade.php
+++ resources/views/TipoMenu/ModalEditar.blade.php
@@ -1,21 +1,23 @@
 <!-- Modal Editar Tipo de Menu-->
-<div class="modal fade" id="ModalEditar{{$tipoMenu->IdTipoMenu}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="ModalEditar{{ $tipoMenu->IdTipoMenu }}" tabindex="-1" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
     <div class="modal-dialog">
-        <div class="modal-content">
+        <div class="modal-content border-0">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Editar Tipo de Menú</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <form action="/EditarTipoMenu/{{$tipoMenu->IdTipoMenu}}" method="POST">
+                <form action="/EditarTipoMenu/{{ $tipoMenu->IdTipoMenu }}" method="POST">
                     @csrf
                     <div class="mb-3">
                         <label for="" class="form-label">Nombre de Tipo de Menú</label>
-                        <input type="text" id="NomTipoMenu" name="NomTipoMenu" class="form-control" tabindex="1" value="{{$tipoMenu->NomTipoMenu}}" onkeyup="mayusculas(this)" required>
+                        <input type="text" id="NomTipoMenu" name="NomTipoMenu" class="form-control rounded"
+                            style="line-height: 18px" tabindex="1" value="{{ $tipoMenu->NomTipoMenu }}"
+                            onkeyup="mayusculas(this)" required>
                     </div>
                     <div>
-                        <label for="" class="form-label">Status</label>
-                        <select name="Status" id="Status" class="form-select">
+                        <label for="" class="form-label">Estatus</label>
+                        <select name="Status" id="Status" class="form-select rounded" style="line-height: 18px">
                             <option {!! $tipoMenu->Status == 0 ? 'selected' : '' !!} value="0">Activo</option>
                             <option {!! $tipoMenu->Status == 1 ? 'selected' : '' !!} value="1">Inactivo</option>
                         </select>
@@ -23,12 +25,8 @@
 
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">
-                    <i class="fa fa-close"></i> Cerrar
-                </button>
-                <button class="btn btn-warning">
-                    <i class="fa fa-edit"></i> Editar
-                </button>
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar </button>
+                <button class="btn btn-warning">Editar </button>
                 </form>
             </div>
         </div>
