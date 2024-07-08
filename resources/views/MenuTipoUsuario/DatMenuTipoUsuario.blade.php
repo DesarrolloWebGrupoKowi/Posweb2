ef56e3d505bfa47bf78f63ebcc618fdb2bc08c54
Daniel Hernandez <hernandezdany41@gmail.com>
2024-07-08 08:51:25 -0700

    modificando vistas de tipos de usuarios

--- resources/views/MenuTipoUsuario/DatMenuTipoUsuario.blade.php
+++ resources/views/MenuTipoUsuario/DatMenuTipoUsuario.blade.php
@@ -1,142 +1,138 @@
 @extends('PlantillaBase.masterbladeNewStyle')
 @section('title', 'Menu Tipo Usuario')
-@section('dashboardWidth', 'width-95')
+@section('dashboardWidth', 'width-general')
 <style>
-    * {
+    /* * {
         scrollbar-width: thin;
         scrollbar-color: #797979 #f1f2f300;
-    }
+    } */
 
-    .over {
+    /* .over {
         overflow: hidden;
         border-radius: 20px;
-    }
+    } */
 </style>
 @section('contenido')
-    <div class="container-fluid pt-4 width-95">
-        <form id="formChange" action="/DatMenuTipoUsuario">
-
-            <div class="d-flex justify-content-sm-between align-items-sm-center flex-column flex-sm-row pb-5">
-                @if (empty($TipoUsuarioFind))
-                    @include('components.title', ['titulo' => 'Asignación de Menús'])
-                @else
-                    @include('components.title', [
-                        'titulo' => 'Asignación de Menús Para ' . $TipoUsuarioFind->NomTipoUsuario,
-                    ])
-                @endif
-                <div class="d-flex align-items-center justify-content-end">
-                    <div class="form-group" style="min-width: 300px">
-                        <label class="fw-bold text-secondary">Tipo de usuario</label>
-                        <select class="form-select " name="IdTipoUsuario" id="IdTipoUsuario" onchange="MenuTipoUsuario()">
-                            <option value="0">Seleccione</option>
-                            @foreach ($tipoUsuarios as $tipoUsuario)
-                                <option {!! $filtroIdTipoUsuario == $tipoUsuario->IdTipoUsuario ? 'selected' : '' !!} value="{{ $tipoUsuario->IdTipoUsuario }}">
-                                    {{ $tipoUsuario->NomTipoUsuario }}</option>
-                            @endforeach
-                        </select>
+    <form id="formChange" action="/DatMenuTipoUsuario">
+        <div class="container-fluid width-general d-flex flex-column gap-4 pt-4">
+            <div class="card border-0 p-4" style="border-radius: 10px">
+                <div class="d-flex justify-content-sm-between align-items-sm-end flex-column flex-sm-row">
+                    @if (empty($TipoUsuarioFind))
+                        @include('components.title', ['titulo' => 'Asignación de Menús'])
+                    @else
+                        @include('components.title', [
+                            'titulo' => 'Asignación de Menús Para ' . $TipoUsuarioFind->NomTipoUsuario,
+                        ])
+                    @endif
+                    <div class="d-flex align-items-center justify-content-end">
+                        <div class="form-group" style="min-width: 300px">
+                            <label class="fw-bold text-secondary">Tipo de usuario</label>
+                            <select class="form-select rounded" style="line-height: 18px" name="IdTipoUsuario"
+                                id="IdTipoUsuario" onchange="MenuTipoUsuario()">
+                                <option value="0">Seleccione tipo de usuario</option>
+                                @foreach ($tipoUsuarios as $tipoUsuario)
+                                    <option {!! $filtroIdTipoUsuario == $tipoUsuario->IdTipoUsuario ? 'selected' : '' !!} value="{{ $tipoUsuario->IdTipoUsuario }}">
+                                        {{ $tipoUsuario->NomTipoUsuario }}</option>
+                                @endforeach
+                            </select>
+                        </div>
                     </div>
+                </div>
+
+                <div>
+                    @include('Alertas.Alertas')
                 </div>
             </div>
 
-            <div>
-                @include('Alertas.Alertas')
-            </div>
-
             <div class="row">
-
                 <div class="col-6">
-                    <div class="d-flex justify-content-sm-between align-items-sm-center flex-column flex-sm-row pb-2">
-                        <h5>Menús Agregados</h5>
-                        <button class="btn btn-sm btn-dark" onclick="remove()">
-                            <i class="fa fa-plus-circle pe-1"></i> Remover
-                        </button>
-                    </div>
-
                     <div class="over">
-                        <div class="content-table content-table-full card p-4 table-responsive"
-                            style="border-radius: 20px; height: 70vh">
+                        <div class="content-table content-table-full card border-0 p-4"
+                            style="border-radius: 10px; height: 70vh">
+                            <h5>Menús Agregados</h5>
+                            {{-- <div
+                                class="d-flex justify-content-sm-between align-items-sm-center flex-column flex-sm-row pb-2">
+                                <button class="btn btn-sm btn-dark" onclick="remove()">
+                                    <i class="fa fa-plus-circle pe-1"></i> Remover
+                                </button>
+                            </div> --}}
                             <table>
                                 <thead class="table-head">
                                     <tr>
                                         <th class="rounded-start">Menús ({{ count($menuTipoUsuarios) }})</th>
+                                        <th></th>
                                         <th class="rounded-end">Seleccionar</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    @if (count($menuTipoUsuarios) == 0)
-                                        @if ($filtroIdTipoUsuario == 0)
-                                            <tr>
-                                                <td colspan="2">Seleccione Tipo de Usuario</td>
-                                            </tr>
-                                        @else
-                                            <tr>
-                                                <td colspan="2">No Hay Menús para este Tipo de Usuario</td>
-                                            </tr>
-                                        @endif
-                                    @else
-                                        @foreach ($menuTipoUsuarios as $menuTipoUsuario)
-                                            <tr>
-                                                <td>{{ $menuTipoUsuario->cmpNomMenu }}</td>
-                                                <td><input class="d-block form-check-input" type="checkbox"
-                                                        name="chkRemoverMenu[]" id="chkRemoverMenu[]"
-                                                        value="{{ $menuTipoUsuario->cmpIdMenu }}">
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                    @endif
+                                    {{-- @include('components.table-empty', [
+                                        'items' => $menuTipoUsuarios,
+                                        'colspan' => 2,
+                                    ]) --}}
+                                    @foreach ($menuTipoUsuarios as $menuTipoUsuario)
+                                        <tr style="font-size: small">
+                                            <td>{{ $menuTipoUsuario->cmpNomMenu }}</td>
+                                            <td>{{ $menuTipoUsuario->NomTipoMenu }}</td>
+                                            <td><input class="d-block form-check-input" type="checkbox"
+                                                    name="chkRemoverMenu[]" id="chkRemoverMenu[]"
+                                                    value="{{ $menuTipoUsuario->cmpIdMenu }}">
+                                            </td>
+                                        </tr>
+                                    @endforeach
                                 </tbody>
                             </table>
+                            <div class="pt-2 d-flex justify-content-end">
+                                <button class="btn btn-sm btn-danger" onclick="remove()"
+                                    {{ count($menuTipoUsuarios) == 0 ? 'disabled' : '' }}>
+                                    Remover @include('components.icons.delete')
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
 
-
                 <div class="col-6">
-                    <div class="d-flex justify-content-sm-between align-items-sm-center flex-column flex-sm-row pb-2">
-                        <h5>Agregar Menús</h5>
-                        <button class="btn btn-sm btn-dark" onclick="agregar()">
-                            <i class="fa fa-plus-circle pe-1"></i> Agregar
-                        </button>
-                    </div>
                     <div class="over">
-                        <div class="content-table content-table-full card p-4 table-responsive"
-                            style="border-radius: 20px; height: 70vh">
+                        <div class="content-table content-table-full card border-0 p-4"
+                            style="border-radius: 10px; height: 70vh">
+                            <h5>Agregar Menús</h5>
                             <table>
                                 <thead class="table-head">
                                     <tr>
                                         <th class="rounded-start">Menús ({{ count($menus) }})</th>
+                                        <th></th>
                                         <th class="rounded-end">Seleccionar</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    @if ($menus == '' || $filtroIdTipoUsuario == 0)
-                                        <tr>
-                                            <td colspan="2">Seleccione Tipo de Usuario</td>
+                                    {{-- @include('components.table-empty', [
+                                        'items' => $menus,
+                                        'colspan' => 2,
+                                    ]) --}}
+                                    @foreach ($menus as $menu)
+                                        <tr style="font-size: small">
+                                            <td>{{ $menu->NomMenu }}</td>
+                                            <td>{{ $menu->NomTipoMenu }}</td>
+                                            <td>
+                                                <input class="d-block form-check-input" type="checkbox"
+                                                    name="chkAgregarMenu[]" id="chkAgregarMenu[]"
+                                                    value="{{ $menu->IdMenu }}">
+                                            </td>
                                         </tr>
-                                    @else
-                                        @if (count($menus) == 0)
-                                            <tr>
-                                                <td colspan="2">No Hay Menús por Agregar</td>
-                                            </tr>
-                                        @endif
-                                        @foreach ($menus as $menu)
-                                            <tr>
-                                                <td>{{ $menu->NomMenu }}</td>
-                                                <td>
-                                                    <input class="d-block form-check-input" type="checkbox"
-                                                        name="chkAgregarMenu[]" id="chkAgregarMenu[]"
-                                                        value="{{ $menu->IdMenu }}">
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                    @endif
+                                    @endforeach
                                 </tbody>
                             </table>
+                            <div class="pt-2 d-flex justify-content-end">
+                                <button class="btn btn-sm btn-warning" onclick="agregar()"
+                                    {{ count($menus) == 0 ? 'disabled' : '' }}>
+                                    Agregar @include('components.icons.plus-circle')
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
 
             </div>
-        </form>
-    </div>
+        </div>
+    </form>
 @endsection
