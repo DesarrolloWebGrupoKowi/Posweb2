d2f617e37287df4405157d1db1236fdc9a2205ea
Daniel Hernandez <hernandezdany41@gmail.com>
2024-07-08 11:53:47 -0700

    modificando vistas de ciudades

--- resources/views/Ciudades/CatCiudades.blade.php
+++ resources/views/Ciudades/CatCiudades.blade.php
@@ -2,32 +2,26 @@
 @section('title', 'Catálogo de Ciudades')
 @section('dashboardWidth', 'width-general')
 @section('contenido')
-    <div class="container-fluid pt-4 width-general">
-        <div class="d-flex justify-content-sm-between align-items-sm-center flex-column flex-sm-row pb-2">
-            @include('components.title', ['titulo' => 'Catálogo de Ciudades'])
-            <div class="">
-                <button type="button" class="btn btn-sm btn-dark" role="tooltip" title="Agregar Usuario"
-                    class="btn btn-default Agregar" data-bs-toggle="modal" data-bs-target="#ModalAgregar">
-                    <i class="fa fa-plus-circle pe-1"></i> Agregar ciudad
-                </button>
-            </div>
-        </div>
+    <div class="container-fluid width-general d-flex flex-column gap-4 pt-4">
 
-        <div>
-            @include('Alertas.Alertas')
-        </div>
-
-        <form class="d-flex align-items-center justify-content-end pb-4 gap-4" action="/CatCiudades" method="get">
-            <div class="input-group" style="max-width: 300px">
-                <input class="form-control" type="text" name="txtFiltro" id="txtFiltro"
-                    placeholder="Escribre el nombre de la ciudad" value="{{ $txtFiltro }}">
-                <div class="input-group-append">
-                    <button class="input-group-text"><span class="material-icons">search</span></button>
+        <div class="card border-0 p-4" style="border-radius: 10px">
+            <div class="d-flex justify-content-sm-between align-items-sm-end flex-column flex-sm-row">
+                @include('components.title', ['titulo' => 'Catálogo de Ciudades'])
+                <div class="">
+                    <button type="button" class="btn btn-sm btn-dark" role="tooltip" title="Agregar Usuario"
+                        class="btn btn-default Agregar" data-bs-toggle="modal" data-bs-target="#ModalAgregar">
+                        Agregar ciudad @include('components.icons.plus-circle')
+                    </button>
                 </div>
             </div>
-        </form>
 
-        <div class="content-table content-table-full card p-4" style="border-radius: 20px">
+            <div>
+                @include('Alertas.Alertas')
+            </div>
+        </div>
+
+        <div class="content-table content-table-full card border-0 p-4" style="border-radius: 10px">
+            @include('components.table-search')
             <table>
                 <thead class="table-head">
                     <tr>
@@ -38,31 +32,26 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @if (count($ciudades) <= 0)
-                        <td colspan="4">No Hay Coincidencias!</td>
-                    @else
-                        @foreach ($ciudades as $ciudad)
-                            <tr>
-                                <td>{{ $ciudad->IdCiudad }}</td>
-                                <td>{{ $ciudad->NomCiudad }}</td>
-                                <td>{{ $ciudad->NomEstado }}</td>
-                                <td>
-                                    <button class="btn btn-sm" data-bs-toggle="modal"
-                                        data-bs-target="#ModalEditar{{ $ciudad->IdCiudad }}"><span
-                                            class="material-icons">edit</span></button>
-                                </td>
-                            </tr>
-                            @include('Ciudades.ModalEditar')
-                        @endforeach
-                    @endif
+                    @include('components.table-empty', ['items' => $ciudades, 'colspan' => 4])
+                    @foreach ($ciudades as $ciudad)
+                        <tr>
+                            <td>{{ $ciudad->IdCiudad }}</td>
+                            <td>{{ $ciudad->NomCiudad }}</td>
+                            <td>{{ $ciudad->NomEstado }}</td>
+                            <td>
+                                <button class="btn-table" data-bs-toggle="modal"
+                                    data-bs-target="#ModalEditar{{ $ciudad->IdCiudad }}">
+                                    @include('components.icons.edit')
+                                </button>
+                            </td>
+                        </tr>
+                        @include('Ciudades.ModalEditar')
+                    @endforeach
                 </tbody>
             </table>
+            @include('components.paginate', ['items' => $ciudades])
         </div>
     </div>
     <!--Modal Agregar Estado-->
     @include('Ciudades.ModalAgregar')
-
-    <div class="mt-5 d-flex justify-content-center">
-        {!! $ciudades->links() !!}
-    </div>
 @endsection
