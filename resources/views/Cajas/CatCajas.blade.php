962d8e084a5b3af9260141843970aa59ae2f776a
Daniel Hernandez <hernandezdany41@gmail.com>
2024-07-08 12:39:51 -0700

    modificando vistas de cajas

--- resources/views/Cajas/CatCajas.blade.php
+++ resources/views/Cajas/CatCajas.blade.php
@@ -2,17 +2,20 @@
 @section('title', 'Catálogo de Cajas')
 @section('dashboardWidth', 'width-general')
 @section('contenido')
-    <div class="container-fluid pt-4 width-general">
-        <div class="d-flex justify-content-sm-between align-items-sm-center flex-column flex-sm-row pb-2">
-            @include('components.title', ['titulo' => 'Catálogo de Cajas'])
-            <div>
-                <button type="button" class="btn btn-sm btn-dark" role="tooltip" title="Agregar Usuario"
-                    class="btn btn-default Agregar" data-bs-toggle="modal" data-bs-target="#ModalAgregar">
-                    <i class="fa fa-plus-circle pe-1"></i> Agregar familia
-                </button>
+    <div class="container-fluid width-general d-flex flex-column gap-4 pt-4">
+
+        <div class="card border-0 p-4" style="border-radius: 10px">
+            <div class="d-flex justify-content-sm-between align-items-sm-end flex-column flex-sm-row">
+                @include('components.title', ['titulo' => 'Catálogo de Cajas'])
+                <div>
+                    <button type="button" class="btn btn-sm btn-dark" role="tooltip" title="Agregar Usuario"
+                        class="btn btn-default Agregar" data-bs-toggle="modal" data-bs-target="#ModalAgregar">
+                        Agregar caja @include('components.icons.plus-circle')
+                    </button>
+                </div>
             </div>
         </div>
-        <div class="content-table content-table-full card p-4" style="border-radius: 20px">
+        <div class="content-table content-table-full card border-0 p-4" style="border-radius: 10px">
             <table>
                 <thead class="table-head">
                     <tr>
--- resources/views/Cajas/ModalAgregar.blade.php
+++ resources/views/Cajas/ModalAgregar.blade.php
@@ -1,26 +1,21 @@
 <!-- Modal Agregar-->
 <div class="modal fade" id="ModalAgregar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
-        <div class="modal-content">
+        <div class="modal-content border-0">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Agregar Caja</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <form action="/CrearCaja" method="GET">
                     <div class="mb-3">
                         <label for="" class="form-label">Número de Caja</label>
-                        <input type="number" id="NumCaja" name="NumCaja" class="form-control" tabindex="1" required
-                            placeholder="Escribe el numero de caja">
+                        <input type="number" id="NumCaja" name="NumCaja" class="form-control rounded"
+                            style="line-height: 18px" tabindex="1" required placeholder="Escribe el numero de caja">
                     </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">
-                    <i class="fa fa-close"></i> Cerrar
-                </button>
-                <button type="submit" class="btn btn-warning">
-                    <i class="fa fa-save"></i> Crear
-                </button>
+                <button type="button" class="btn btn-sm btn-danger" data-bs-dismiss="modal">Cerrar </button>
+                <button type="submit" class="btn btn-sm btn-warning">Crear </button>
                 </form>
             </div>
         </div>
