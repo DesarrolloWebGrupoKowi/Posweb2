16cdbee1db0f567b57e34f3bb4917990e4a98f6d
Daniel Hernandez <hernandezdany41@gmail.com>
2024-07-08 15:50:53 -0700

    modificando vistas de tipo pago por tienda, ajuste del cambio anterior

--- resources/views/TipoPagoTienda/DatTipoPagoTienda.blade.php
+++ resources/views/TipoPagoTienda/DatTipoPagoTienda.blade.php
@@ -25,9 +25,9 @@
             </div>
         </div>
 
-        <div class="accordion" id="accordionExample">
-            @foreach ($tiposPagoTienda as $tipoPagoTienda)
-                <div class="accordion-item border-0" style="border-radius: 10px; overflow: hidden;">
+        {{-- <div class="accordion" id="accordionExample"> --}}
+        @foreach ($tiposPagoTienda as $tipoPagoTienda)
+            {{-- <div class="accordion-item border-0" style="border-radius: 10px; overflow: hidden;">
                     <h2 class="accordion-header" id="headingOne">
                         <button class="accordion-button" type="button" data-bs-toggle="collapse"
                             data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
@@ -35,78 +35,77 @@
                         </button>
                     </h2>
                     <div id="collapseOne" class="accordion-collapse collapse show shadow" aria-labelledby="headingOne"
-                        data-bs-parent="#accordionExample">
-                        <div class="row">
-                            <div class="col-6">
-                                <div class="accordion-body">
-                                    <form action="/RemoverDatTipoPagoTienda">
-                                        <input type="hidden" name="idTienda" value="{{ $idTienda }}">
-                                        <ul class="list-group">
-                                            <li class="list-group-item text-white" style="background: #1e293b">
-                                                Tipos de Pago
-                                            </li>
-                                            @if ($tipoPagoTienda->TiposPago->count() == 0)
-                                                <li class="list-group-item">
-                                                    <i class="fa fa-plus"></i> Agrega un Tipo de Pago
-                                                </li>
-                                            @else
-                                                @foreach ($tipoPagoTienda->TiposPago as $tPago)
-                                                    <li class="list-group-item">
-                                                        <input class="form-check-input me-1" type="checkbox"
-                                                            name="chkIdTipoPagoRemove[]" value="{{ $tPago->IdTipoPago }}">
-                                                        {{ $tPago->NomTipoPago }}
-                                                    </li>
-                                                @endforeach
-                                            @endif
-                                        </ul>
-                                        <div class="mt-2 d-flex justify-content-center">
-                                            <button {!! $tipoPagoTienda->TiposPago->count() == 0 ? 'hidden' : '' !!}
-                                                class="btn
+                        data-bs-parent="#accordionExample"> --}}
+            <div class="row">
+                <div class="col-6">
+                    <div class="card border-0 p-4" style="border-radius: 10px">
+                        <form action="/RemoverDatTipoPagoTienda">
+                            <input type="hidden" name="idTienda" value="{{ $idTienda }}">
+                            <ul class="list-group">
+                                <li class="list-group-item text-white" style="background: #1e293b">
+                                    Tipos de Pago
+                                </li>
+                                @if ($tipoPagoTienda->TiposPago->count() == 0)
+                                    <li class="list-group-item border-0 text-secondary" style="font-weight: 500">
+                                        <i class="fa fa-plus"></i> Agrega un Tipo de Pago
+                                    </li>
+                                @else
+                                    @foreach ($tipoPagoTienda->TiposPago as $tPago)
+                                        <li class="list-group-item border-0 text-secondary" style="font-weight: 500">
+                                            <input class="form-check-input me-1" type="checkbox"
+                                                name="chkIdTipoPagoRemove[]" value="{{ $tPago->IdTipoPago }}">
+                                            {{ $tPago->NomTipoPago }}
+                                        </li>
+                                    @endforeach
+                                @endif
+                            </ul>
+                            <div class="mt-2 d-flex justify-content-center">
+                                <button {!! $tipoPagoTienda->TiposPago->count() == 0 ? 'hidden' : '' !!}
+                                    class="btn
                                         btn-danger">
-                                                <i class="fa fa-minus-circle"></i> Remover
-                                            </button>
-                                        </div>
-                                    </form>
-                                </div>
+                                    <i class="fa fa-minus-circle"></i> Remover
+                                </button>
                             </div>
-                            <div class="col-6">
-                                <div class="accordion-body">
-                                    <form action="/AgregarDatTipoPagoTienda">
-                                        <input type="hidden" name="idTienda" value="{{ $idTienda }}">
-                                        <ul class="list-group">
-                                            <li class="list-group-item text-white"style="background: #1e293b">
-                                                Agregar Tipos de Pago
-                                            </li>
-                                            @if ($tiposPagoFaltantes->count() == 0)
-                                                <li class="list-group-item">
-                                                    <i class="fa fa-check"></i> No Hay Tipos de Pago por Agregar
-                                                </li>
-                                            @else
-                                                @foreach ($tiposPagoFaltantes as $tPagoFaltante)
-                                                    <li class="list-group-item">
-                                                        <input class="form-check-input me-1" type="checkbox"
-                                                            name="chkIdTipoPagoAdd[]"
-                                                            value="{{ $tPagoFaltante->IdTipoPago }}">
-                                                        {{ $tPagoFaltante->NomTipoPago }}
-                                                    </li>
-                                                @endforeach
-                                            @endif
-                                        </ul>
-                                        <div class="mt-2 d-flex justify-content-center">
-                                            <button {!! $tiposPagoFaltantes->count() == 0 ? 'hidden' : '' !!}
-                                                class="btn
-                                        btn-warning">
-                                                <i class="fa fa-plus-circle"></i> Agregar
-                                            </button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
-            @endforeach
-        </div>
+                <div class="col-6">
+                    <div class="card border-0 p-4" style="border-radius: 10px">
+                        <form action="/AgregarDatTipoPagoTienda">
+                            <input type="hidden" name="idTienda" value="{{ $idTienda }}">
+                            <ul class="list-group">
+                                <li class="list-group-item text-white"style="background: #1e293b">
+                                    Agregar Tipos de Pago
+                                </li>
+                                @if ($tiposPagoFaltantes->count() == 0)
+                                    <li class="list-group-item border-0 text-secondary" style="font-weight: 500">
+                                        <i class="fa fa-check"></i> No Hay Tipos de Pago por Agregar
+                                    </li>
+                                @else
+                                    @foreach ($tiposPagoFaltantes as $tPagoFaltante)
+                                        <li class="list-group-item border-0 text-secondary" style="font-weight: 500">
+                                            <input class="form-check-input me-1" type="checkbox" name="chkIdTipoPagoAdd[]"
+                                                value="{{ $tPagoFaltante->IdTipoPago }}">
+                                            {{ $tPagoFaltante->NomTipoPago }}
+                                        </li>
+                                    @endforeach
+                                @endif
+                            </ul>
+                            <div class="mt-2 d-flex justify-content-center">
+                                <button {!! $tiposPagoFaltantes->count() == 0 ? 'hidden' : '' !!}
+                                    class="btn
+                                        btn-warning">
+                                    <i class="fa fa-plus-circle"></i> Agregar
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            {{-- </div>
+                </div> --}}
+        @endforeach
+        {{-- </div> --}}
     </div>
 
     <script>
