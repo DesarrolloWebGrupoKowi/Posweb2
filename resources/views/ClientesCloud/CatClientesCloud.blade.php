d6d4eb42af72923876540a42c0f7b4133f916b01
Daniel Hernandez <hernandezdany41@gmail.com>
2024-07-08 12:58:55 -0700

    modificando vistas de cloud clientes

--- resources/views/ClientesCloud/CatClientesCloud.blade.php
+++ resources/views/ClientesCloud/CatClientesCloud.blade.php
@@ -2,20 +2,23 @@
 @section('title', 'Catálogo de Clientes Cloud')
 @section('dashboardWidth', 'width-general')
 @section('contenido')
-    <div class="container-fluid pt-4 width-general">
-        <div class="d-flex justify-content-sm-between align-items-sm-center flex-column flex-sm-row pb-2">
-            @include('components.title', ['titulo' => 'Catálogo de Clientes Cloud'])
-            <div>
-                <button type="button" class="btn btn-sm btn-dark" role="tooltip" title="Agregar Usuario"
-                    class="btn btn-default Agregar" data-bs-toggle="modal" data-bs-target="#ModalAgregar">
-                    <i class="fa fa-plus-circle pe-1"></i> Agregar familia
-                </button>
+    <div class="container-fluid width-general d-flex flex-column gap-4 pt-4">
+
+        <div class="card border-0 p-4" style="border-radius: 10px">
+            <div class="d-flex justify-content-sm-between align-items-sm-end flex-column flex-sm-row">
+                @include('components.title', ['titulo' => 'Catálogo de Clientes Cloud'])
+                <div>
+                    <button type="button" class="btn btn-sm btn-dark" role="tooltip" title="Agregar Usuario"
+                        class="btn btn-default Agregar" data-bs-toggle="modal" data-bs-target="#ModalAgregar">
+                        Agregar familia @include('components.icons.plus-circle')
+                    </button>
+                </div>
+            </div>
+            <div class="">
+                @include('Alertas.Alertas')
             </div>
         </div>
-        <div class="">
-            @include('Alertas.Alertas')
-        </div>
-        <div class="content-table content-table-full card p-4" style="border-radius: 20px">
+        <div class="content-table content-table-full card border-0 p-4" style="border-radius: 10px">
             <table>
                 <thead class="table-head">
                     <tr>
@@ -30,7 +33,7 @@
                 <tbody>
                     @if ($clientesCloud->count() == 0)
                         <tr>
-                            <td colspan="3">No Hay Coincidencias!</td>
+                            <td colspan="6">No Hay Coincidencias!</td>
                         </tr>
                     @else
                         @foreach ($clientesCloud as $clienteCloud)
--- resources/views/ClientesCloud/ModalAgregar.blade.php
+++ resources/views/ClientesCloud/ModalAgregar.blade.php
@@ -1,21 +1,21 @@
 <!-- Modal Agregar-->
-<div class="modal fade" id="ModalAgregar" data-bs-backdrop="static" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="ModalAgregar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
-        <div class="modal-content">
+        <div class="modal-content border-0">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Agregar Cliente Cloud</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <form action="/BuscarCustomer" target="ifrBuscarCustomer">
                     <div class="mb-3">
                         <div class="row">
                             <div class="col-4">
-                                <input type="text" class="form-control" name="txtFiltro" required>
+                                <input type="text" class="form-control rounded" style="line-height: 18px"
+                                    name="txtFiltro" required>
                             </div>
                             <div class="col-2">
-                                <button class="btn">
-                                    <span class="material-icons">search</span>
+                                <button class="btn btn-dark-outline">
+                                    @include('components.icons.search')
                                 </button>
                             </div>
                         </div>
@@ -24,9 +24,7 @@
                 <iframe name="ifrBuscarCustomer" width="100%" height="300vh"></iframe>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">
-                    <i class="fa fa-close"></i> Cerrar
-                </button>
+                <button type="button" class="btn btn-sm btn-danger" data-bs-dismiss="modal"> Cerrar </button>
             </div>
         </div>
     </div>
