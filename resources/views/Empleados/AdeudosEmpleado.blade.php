71caed3ed7bcba97c7c484558fed16105aac862c
Daniel Hernandez <hernandezdany41@gmail.com>
2024-08-13 12:58:04 -0700

    add: agregando adeudo externo en reporte de adeudos por empleado

--- resources/views/Empleados/AdeudosEmpleado.blade.php
+++ resources/views/Empleados/AdeudosEmpleado.blade.php
@@ -2,17 +2,17 @@
 @section('title', 'Reporte de Adeudos')
 @section('dashboardWidth', 'width-general')
 @section('contenido')
-    <div class="container-fluid width-general d-flex flex-column gap-4 pt-4">
+    <div class="gap-4 pt-4 container-fluid width-general d-flex flex-column">
 
-        <div class="card border-0 p-4" style="border-radius: 10px">
+        <div class="p-4 border-0 card" style="border-radius: 10px">
             <div class="d-flex justify-content-sm-between align-items-sm-end flex-column flex-sm-row">
                 @include('components.title', ['titulo' => 'Adeudos Por Empleado'])
             </div>
         </div>
 
-        <div class="content-table content-table-full card border-0 p-4" style="border-radius: 10px">
+        <div class="p-4 border-0 content-table content-table-full card" style="border-radius: 10px">
             <div class="d-flex justify-content-between">
-                <div class="d-flex align-items-center gap-2">
+                <div class="gap-2 d-flex align-items-center">
                     @if (count($adeudo) == 0 && !empty($txtFiltro))
                         <h5>
                             @include('components.icons.info') No se Encontro el Empleado
@@ -37,6 +37,9 @@
                 </thead>
                 <tbody>
                     @include('components.table-empty', ['items' => $adeudo, 'colspan' => 3])
+                    @php
+                        $adeudoTotal = 0;
+                    @endphp
                     @foreach ($adeudo as $aEmpleado)
                         @foreach ($aEmpleado->Adeudos as $adeudoEmpleado)
                             <tr>
@@ -44,6 +47,23 @@
                                 <td>{{ strftime('%d %B %Y, %H:%M', strtotime($adeudoEmpleado->FechaVenta)) }}</td>
                                 <td>${{ number_format($adeudoEmpleado->ImporteCredito, 2) }}</td>
                             </tr>
+                            @php
+                                $adeudoTotal += $adeudoEmpleado->ImporteCredito;
+                            @endphp
+                        @endforeach
+                    @endforeach
+                    @foreach ($adeudo as $aEmpleado)
+                        @foreach ($aEmpleado->VentasExternas as $externo)
+                            <tr>
+                                <td>
+                                    <span class="tags-red">ADEUDO EXTERNO</span>
+                                </td>
+                                <td>{{ strftime('%d %B %Y, %H:%M', strtotime($externo->FechaVenta)) }}</td>
+                                <td>${{ number_format($externo->CreditoActual, 2) }}</td>
+                            </tr>
+                            @php
+                                $adeudoTotal += $externo->CreditoActual;
+                            @endphp
                         @endforeach
                     @endforeach
                 </tbody>
