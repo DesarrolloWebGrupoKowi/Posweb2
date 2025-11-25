1b00ca057e1753c83fe630c7e15345142b39463d
Daniel Hernandez <hernandezdany41@gmail.com>
2025-11-25 15:54:30 -0700

    Agregando pulso, y que pueda subir ventas al momento de hacer la venta con un job

---
+++ app/Jobs/SubirVentaJob.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class SubirVentaJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        try {
+            // Ejecutar tu procedimiento almacenado en SQL Server
+            Log::info("Sp_Subida_Ventas se va a ejecutar.");
+            DB::statement("EXEC Sp_Subida_Ventas");
+
+            Log::info("Sp_Subida_Ventas ejecutado correctamente.");
+        } catch (\Throwable $e) {
+            Log::error("Error al ejecutar Sp_Subida_Ventas: " . $e->getMessage());
+            throw $e; // Para que Laravel marque el job como fallido si aplica
+        }
+    }
+}
