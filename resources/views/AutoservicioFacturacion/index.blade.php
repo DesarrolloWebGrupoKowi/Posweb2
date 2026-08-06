3a0f1c048aba5e89be41d5d51791d6c71b98dea6
Daniel Hernandez <hernandezdany41@gmail.com>
2026-08-06 11:23:54 -0700

    habilitando boton para enviar pedido a oracle

--- resources/views/AutoservicioFacturacion/index.blade.php
+++ resources/views/AutoservicioFacturacion/index.blade.php
@@ -1685,7 +1685,7 @@
             btn.style.opacity = '1';
 
             // Llamar a la API de Oracle
-            fetch(`http://qaoracleorderrest/api/SalesOrder/PostSales?OrdenVta=${folio}&Origen=AUT`, {
+            fetch(`https://oracleorderrest/api/SalesOrder/PostSales?OrdenVta=${folio}&Origen=AUT`, {
                     method: 'GET',
                     headers: {
                         'Accept': 'application/json'
