45e1892a4d3bdc38e034b7d9ede022d47945ced1
Daniel Hernandez <hernandezdany41@gmail.com>
2026-08-06 11:45:41 -0700

    ajustando url de enviar a oracle a prod

--- resources/views/AutoservicioFacturacion/index.blade.php
+++ resources/views/AutoservicioFacturacion/index.blade.php
@@ -1685,7 +1685,7 @@
             btn.style.opacity = '1';
 
             // Llamar a la API de Oracle
-            fetch(`https://oracleorderrest.kowi.com.mx/api/SalesOrder/PostSales?OrdenVta=${folio}&Origen=AUT`, {
+            fetch(`https://oracleordenrest.kowi.com.mx/api/SalesOrder/PostSales?OrdenVta=${folio}&Origen=AUT`, {
                     method: 'GET',
                     headers: {
                         'Accept': 'application/json'
